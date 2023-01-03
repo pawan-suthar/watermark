b13f1488de24099d3fe52e720aad2da3d91cef75
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-01-03 15:42:37 +0530

    fghfh

--- watermark/settings.py
+++ watermark/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-p+%cn@r03d1z&jcogh=&g58w2cf+_cajd(7m0_@r60)#%i%_os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,8 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "public/static")
 MEDIA_URL = 'media/'
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
